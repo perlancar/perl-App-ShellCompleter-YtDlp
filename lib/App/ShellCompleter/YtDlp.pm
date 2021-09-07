8e4b54640e0a283663f37c129519c275d932f5e1
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-09-07 21:50:16 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/YtDlp.pm
@@ -0,0 +1,22 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
+package App::ShellCompleter::YtDlp;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for yt-dlp
+
+=head1 SYNOPSIS
+
+See L<_yt-dlp> included in this distribution.
+
+
+=head1 SEE ALSO
+
+L<App::ShellCompleter::YoutubeDl> for the C<youtube-dl> CLI.
+
+yt-dlp, L<https://github.com/yt-dlp/yt-dlp>
